d86506dfef22850545c68e9bfe8d35621da2610c
JamalKhan-sys <73995201+JamalKhan-sys@users.noreply.github.com>
2020-11-05 08:23:36 -0800

    Update UpdateAndDelete.sql

--- UpdateAndDelete.sql
+++ UpdateAndDelete.sql
@@ -1,12 +1,62 @@
-Select Count(*) As [Number of std]
-  FROM [StudentTranscriptDB].[dbo].[Transcript]
+create database StudentTranscriptDB
+use StudentTranscriptDB
 
-  Group By CourseName
 
-  Select CourseName, AVG(GPA) As [AVG GPA]
-  FROM [StudentTranscriptDB].[dbo].[Transcript]
+create table Student(
+StudentId nchar(30),
+Name(30))
 
-  Group By CourseName
+  insert into Student(StudentId,Name) values 
+  ('fa20-bcs-001','ali')
+ insert into Student(StudentId,Name) values 
+  ('fa20-bcs-002','Aftab')
+ insert into Student(StudentId,Name) values 
+ ('fa20-bcs-003','Ammar')
+  insert into Student(StudentId,Name) values 
+('fa20-bcs-004','Mohsin')
+insert into Student(StudentId,Name) values 
+('fa20-bcs-005','Zubair')
+insert into Student(StudentId,Name) values 
+('fa20-bcs-006','Daniyal')
+insert into Student(StudentId,Name) values 
+('fa20-bcs-007','Usman')
+insert into Student(StudentId,Name) values 
+('fa20-bcs-008','Abbass')
+insert into Student(StudentId,Name) values 
+('fa20-bcs-009','Saddique')
+insert into Student(StudentId,Name) values 
+('fa20-bcs-010','Umar')
+
+
+
+create table Transcript(
+CourseName nchar(30),
+GPA float(30),
+StudentId nchar(30)
+);
+
+  insert into Transcript (CourseName,GPA,StudentId) values
+  ('Database Systems','1.3','fa20-bcs-001')
+insert into Transcript (CourseName,GPA,StudentId) values
+  ('Database Systems','1.3','fa20-bcs-002')
+ insert into Transcript (CourseName,GPA,StudentId) values
+  ('Database System','2.7','fa20-bcs-003')
+insert into Transcript (CourseName,GPA,StudentId) values
+  ('Database Systems','3.3','fa20-bcs-004')
+insert into Transcript (CourseName,GPA,StudentId) values
+  ('Database System','4.0','fa20-bcs-005')
+insert into Transcript (CourseName,GPA,StudentId) values
+  ('Operating Systems','2.7','fa20-bcs-006')
+insert into Transcript (CourseName,GPA,StudentId) values
+  ('Database Systems','2.0','fa20-bcs-007')
+insert into Transcript (CourseName,GPA,StudentId) values
+  ('Database Systems','3.7','fa20-bcs-008')
+insert into Transcript (CourseName,GPA,StudentId) values
+  ('Operating Systems','2.5','fa20-bcs-009')
+insert into Transcript (CourseName,GPA,StudentId) values
+  ('Database Systems','3.0','fa20-bcs-010')
+
+
 
   DELETE FROM Transcript WHERE StudentId='fa20-bcs-008';
   select * from Transcript
@@ -31,3 +81,4 @@ Select Count(*) As [Number of std]
 
   UPDATE Transcript set GPA='0'WHERE StudentId='fa20-bcs-009';
   select * from Transcript
+
